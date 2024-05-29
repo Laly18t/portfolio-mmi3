c48daf011f79db4cfb469a9d755706595e2825a1
Laly18t <laly.teissier@gmail.com>
2024-05-29 17:15:34 +0200

    add text Mentionslegales

--- src/pages/MentionsLegales.jsx
+++ src/pages/MentionsLegales.jsx
@@ -2,52 +2,71 @@ function MentionsLegales() {
 
     return (
         <div className="wrap">
-            <h1 className="titre text-center text-2xl stroke m-8">MENTIONS LEGALES </h1>
+            <h1 className="titreHomePart4 text-center text-4xl stroke m-8">MENTIONS LEGALES </h1>
 
-            <p>
-                Les présentes conditions générales fixent les modalités d’utilisation du service web https://lalyteissier.fr
-                <br></br>
-                L’utilisateur reconnait avoir pris connaissance de ce document et accepté l’ensemble de ces informations, que cet usage soit fait à titre personnel ou professionnel. Si cet usage est fait à titre professionnel, l’utilisateur garantit détenir les pouvoirs nécessaires pour accepter ces conditions générales au sein de son organisation.
-            </p>
 
-            <h2 className="mt-4 titre stroke">CRÉATEUR, ÉDITEUR ET PROPRIÉTAIRE DU SITE</h2>
+            <h2 className="mt-4 titre stroke text-2xl">1 – ÉDITION DU SITE</h2>
             <p>
-                Directeur de publication : Laly Teissier.
+                En vertu de l’article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans l’économie numérique, il est précisé aux utilisateurs du site internet www.lalyteissier.fr l’identité des différents intervenants dans le cadre de sa réalisation et de son suivi :
                 <br></br>
-                E-mail : laly.teissier[at]gmail.com
                 <br></br>
-                Site web : www.lalyteissier.com
+                Propriétaire du site :
+                <br></br>
+                Contact : laly.teissier[at]gmail.com - 07 82 91 09 75
+                <br></br>
+                Adresse :
+                <br></br>
+                Site web : www.lalyteissier.fr
                 <br></br>
                 Crédits photo : Laly Teissier.
-            </p>
-            <h2 className="mt-4 titre stroke">HÉBERGEUR</h2>
-            <p>
-                Ce site est hébergé par la société ---, S.A.S au capital de --- €.
+                <br></br>
+                Hébergeur :
+                <br></br>
+                NOM
+                <br></br>
+                S.A.S au capital de --- €.
                 <br></br>
                 Siège social : ----
                 <br></br>
-                Tel : --- (prix d'un appel vers un poste fixe en France).
+                Tel : --- .
+                <br></br>
+                www.
+                <br></br>
+                <br></br>
+                Réalisation du site internet :
+                <br></br>
+                Laly Teissier
+                <br></br>
+
             </p>
 
-            <h2 className="mt-4 titre stroke">CONDITIONS D’UTILISATION DU SITE</h2>
-            <p>Le présent site internet est la propriété exclusive de « Laly Teissier ». Les éléments du site, tels que les textes, les images fixes ou animées, les bases de données, les programmes, les langages,… etc, sont protégés par toute législation en vigueur (le droit d’auteur, le droit suis generis du producteur de la base de données, droit des marques…). L’autorisation de reproduction ne vous est concédée que sous forme numérique en consultation. L’impression papier est autorisée aux fins de copie privée à l’usage exclusif du copiste au sens de l’article L. 122-5 2° du Code de la Propriété Intellectuelle. Les informations contenues dans le présent site internet sont données à titre indicatif et sont sujettes à changement sans préavis. Nous ne pouvons garantir l’exactitude des informations qu’il contient, ni le fonctionnement du site internet dans tous les cas de figure. Nous n’assumons aucune responsabilité relative aux informations contenues et à l’existence ou la disponibilité de toute offre mentionnée dans le présent site internet et déclinons toute responsabilité découlant d’une négligence ou autre concernant ces informations.</p>
+            <h2 className="mt-4 titre stroke text-2xl">2 – PROPRIÉTÉ INTELLECTUELLE ET CONTREFAÇONS</h2>
+            <p>
+                Laly Teissier est propriétaire des droits de propriété intellectuelle et détient les droits d’usage sur tous les éléments accessibles sur le site internet, notamment les textes, images, graphismes, logos, vidéos, architecture, icônes et sons. Toute reproduction, représentation, modification, publication, adaptation de tout ou partie des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable de Laly Teissier. Toute exploitation non autorisée du site ou de l’un quelconque des éléments qu’il contient sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
+            </p>
 
-            <h2 className="mt-4 titre stroke">DROITS DE PROPRIÉTÉ INTELLECTUELLE</h2>
-            <p>L’ensemble des éléments de ce site est protégé par copyright © – tous droits réservés. Tous les éléments de propriété intellectuelle, marques, noms commerciaux et logos sont la propriété de « Laly Teissier » sauf mentions contraires. Les dessins, photographies, images, textes, séquences animées sonores ou non, et autres documentations représentés sur le présent site internet sont objet de droits de propriété industrielle et/ou intellectuelle, propriétés de « Laly Teissier ». À ce titre, toute reproduction, représentation, adaptation, traduction, et/ou transformation, partielle ou intégrale, ou transfert sur un autre site sont interdits. La copie à usage privé de ces différents objets de droit est autorisée, sous réserve de la mention de la source. Le contenu du site est proposé pour un usage privé du visiteur et aucune partie de son contenu ne doit être redistribuée, republiée ou stockée sous quelque format que ce soit sans l’autorisation expresse du Concepteur. Le contenu de ce site (images et textes) est protégé par la loi n° 57-298 du 11 mars 1957 sur la propriété artistique (J.O. du 14 mars 1957) et les droits des auteurs. Tout différent lié à l’interprétation, l’utilisation ou le contenu du présent site internet sera soumis à la compétence exclusive du tribunal compétent relevant du siège de l'éditeur de ce site.</p>
+            <h2 className="mt-4 titre stroke text-2xl">3 – LIMITATIONS DE RESPONSABILITÉ</h2>
+            <p>
+                Laly Teissier ne pourra être tenu pour responsable des dommages directs et indirects causés au matériel de l’utilisateur, lors de l’accès au site www.lalyteissier.fr.
+                <br></br>
+                Laly Teissier décline toute responsabilité quant à l’utilisation qui pourrait être faite des informations et contenus présents sur www.lalyteissier.fr.
+                <br></br>
+                Laly Teissier s’engage à sécuriser au mieux le site www.lalyteissier.fr, cependant sa responsabilité ne pourra être mise en cause si des données indésirables sont importées et installées sur son site à son insu.
+                <br></br>
+                Des espaces interactifs (espace contact) sont à la disposition des utilisateurs. Laly Teissier se réserve le droit de supprimer, sans mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la législation applicable en France, en particulier aux dispositions relatives à la protection des données. Le cas échéant, Laly Teissier se réserve également la possibilité de mettre en cause la responsabilité civile et/ou pénale de l’utilisateur, notamment en cas de message à caractère raciste, injurieux, diffamant, ou pornographique, quel que soit le support utilisé (texte, photographie …).
+            </p>
+
+            <h2 className="mt-4 titre stroke text-2xl">4 – CNIL ET GESTION DES DONNÉES PERSONNELLES</h2>
+            <p>Conformément aux dispositions de la loi 78-17 du 6 janvier 1978 modifiée, l’utilisateur du site www.lalyteissier.fr dispose d’un droit d’accès, de modification et de suppression des informations collectées. Pour exercer ce droit, envoyez un message via notre formulaire de contact.</p>
 
 
-            <h2 className="mt-4 titre stroke">LIMITATION DE RESPONSABILITÉ</h2>
-            <p>Les renseignements et présentations contenus dans les pages de ce site sont fournis à titre purement indicatif et restent susceptibles de modifications à tout moment et sans préavis. Leur utilisation à des fins privées ou professionnelles ne saurait engager la responsabilité de l'éditeur du présent site internet de quelque manière que ce soit. Il ne peut non plus garantir l’exactitude, la précision ou l’exhaustivité des informations mises à disposition sur ce site. Les photos ne sont pas contractuelles. En aucun cas, l’éditeur ni l’hébergeur du site ne peuvent être tenus responsables de dommages directs ou indirect, y compris mais de façon non limitative, de la perte de bénéfice, de clientèle ou de données, dus à l’utilisation ou de l’impossibilité d’utiliser le site et son contenu, ainsi que les éléments auxquels le site permet d’accéder. Cette limitation vaut, tant en matière contractuelle que délictuelle, même si l’éditeur et/ou l’hébergeur ont été informé de la possibilité d’un tel dommage. L’éditeur de ce site internet ne peut garantir que le site sera en permanence en ligne et s’accorde le droit, à sa complète discrétion, de suspendre l’accès de tout ou partie du contenu du site.</p>
+            <h2 className="mt-4 titre stroke text-2xl">5 – LIENS HYPERTEXTES ET COOKIES</h2>
+            <p>Le site www.lalyteissier.fr contient des liens hypertextes vers d’autres sites et dégage toute responsabilité à propos de ces liens externes ou des liens créés par d’autres sites vers www.lalyteissier.fr.
+                <br></br>
+                La navigation sur le site www.lalyteissier.fr est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de l’utilisateur. Un « cookie » est un fichier de petite taille qui enregistre des informations relatives à la navigation d’un utilisateur sur un site. Les données ainsi obtenues permettent d’obtenir des mesures de fréquentation, par exemple. Vous avez la possibilité d’accepter ou de refuser les cookies en modifiant les paramètres de votre navigateur. Aucun cookie ne sera déposé sans votre consentement. Les cookies sont enregistrés pour une durée maximale de 1 mois.</p>
 
-            <h2 className="mt-4 titre stroke">LOI APPLICABLE ET JURIDICTION</h2>
-            <p>Le présent site, dans tous ses éléments, est régi par la loi française. Conformément à la loi du 6 Août 2004 dite “informatique et libertés”, les données que vous saisissez dans les différents formulaires ou liens email, sont destinées à l’éditeur de ce site internet et demeurent strictement confidentielles. Vous disposez d’un droit d’accès, de modification, de rectification et de suppression des données qui vous concernent (art.34 de la loi « Informatique et Libertés ») par simple demande envoyée par mail. L’éditeur de ce site internet est domicilié en France. Tout sujet concernant ce site est régi par la loi Française sous juridiction d’un tribunal français. Bien que ce site soit accessible en dehors du territoire français, tout visiteur est responsable de son utilisation en accord avec les lois et réglementations françaises et celles de son pays incluant, mais non limités, aux chargements informatiques d’informations.</p>
-
-            <h2 className="mt-4 titre stroke">LIENS HYPERTEXTES</h2>
-            <p>La présence de liens hypertexte renvoyant vers d’autres sites internet ne constitue pas une garantie sur la qualité de contenu et de bon fonctionnement desdits sites. Notre responsabilité ne saurait être engagée quant au contenu de ces sites. L’internaute doit utiliser ces informations avec les précautions d’usage. La création de liens hypertexte vers nos sites est soumise à notre accord préalable. Dans le cas de la création d’un lien hypertexte vers nos sites qui n’aurait pas fait l’objet d’un accord préalable, nous nous réservons le droit d’exiger à tout moment le retrait dudit lien hypertexte pointant sur nos sites.</p>
-
-            <h2 className="mt-4 titre stroke">LIENS HYPERTEXTES VERS DES SITES TIERS
-            </h2>
-            <p>L'éditeur du présent site internet n’a pas vérifié l’intégralité des sites qui peuvent être liés au sien et ne peut être tenu responsable du contenu des sites vers lesquels des liens sont faits. Leur présence ne signifie en aucune manière que l'éditeur du présent site internet adhère à leur contenu ou accepte une responsabilité quelconque pour le contenu ou l’utilisation de cet autre site Internet. La responsabilité de l'éditeur du présent site internet ne peut être engagée pour le contenu erroné ou inexact, diffamatoire, injurieux, calomnieux, mensonger, obscène, pornographique, blasphématoire de l’un ou l’autre de ces sites de tiers. Vous accédez par conséquent au contenu de ces sites extérieurs sous votre seule et entière responsabilité. Les liens proposés par l'éditeur du présent site internet, sauf mention contraire, ne sont en aucune façon une forme de promotion ou de publicité. Ils sont proposés car soit ils sont les sources d’une information soit ils offrent des informations supplémentaires et complémentaire à celles fournies sur le site présent. Ces liens sont proposés pour l’agrément des visiteurs seulement et l’éditeur ne prend aucune responsabilité quant à l’utilisation ou aux conséquences qui pourraient découler de la connection. L’éditeur a pris des précautions raisonnables pour sa protection contre des virus informatiques mais ne peut garantir que ce site ou tout autre site en lien soit complétement sécurisé de tous les virus ou de tout autre dommage qui pourrait endommagé les systèmes des visiteurs. Les visiteurs doivent s’assurer auparavant qu’ils sont eux mêmes suffisamment protégés par leurs propres logiciels de protection.</p>
+            <h2 className="mt-4 titre stroke text-2xl">6 - DROIT APPLICABLE ET ATTRIBUTION DE JURIDICTION</h2>
+            <p>Tout litige en relation avec l’utilisation du site www.lalyteissier.fr est soumis au droit français. En dehors des cas où la loi ne le permet pas, il est fait attribution exclusive de juridiction aux tribunaux compétents de Chambéry.</p>
 
         </div>
     )
