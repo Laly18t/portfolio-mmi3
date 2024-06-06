af03fcfb949e1313a9aa8c68ccea4e8b848257b0
Laly18t <laly.teissier@gmail.com>
2024-06-06 09:44:10 +0200

    responsive design

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,11 +2,11 @@ function Contact() {
     return (
         <div className="homePart6 home wrap">
             <h2 id="contactLink" className="titreHomePart6 stroke">Contactez-moi !</h2>
-            <div className="flex justify-around mt-8 mb-8">
-                <div className="contactPhone">
+            <div className="flex sm:flex-row flex-col items-center justify-around mt-8 mb-8">
+                <div className="contactPhone m-4">
                     <p className="text-2xl text-beigee">07 82 91 09 75</p>
                 </div>
-                <div className="contactMail">
+                <div className="contactMail m-4">
                     <p className="text-2xl text-beigee text-center stroke-terracota stroke-1">laly.teissier <br />@gmail.com</p>
                 </div>
             </div>
