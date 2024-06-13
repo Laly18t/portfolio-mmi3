eb9dc5164316afa0d9e325160e8f5080d565c3b4
Laly18t <laly.teissier@gmail.com>
2024-06-13 16:41:56 +0200

    fix: add photo and responsive design

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -6,6 +6,11 @@ import sun from "../assets/sun.svg";
 import link from "../assets/external-link.svg";
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
+import myRole1 from "../assets/photo/myRole1.jpeg";
+import myRole2 from "../assets/photo/myRole2.jpeg";
+import myRole3 from "../assets/photo/myRole3.jpg";
+
+
 function Project() {
     let { projectId } = useParams();
 
@@ -21,12 +26,12 @@ function Project() {
 
             <TopPage page="projet" titreText={projectData.class} text={projectData.short_description} />
 
-            <div className="flex wrap">
+            <div className="flex flex-col md:flex-row wrap">
                 <div className="w-1/3 h-fit m-4 bg-vertt z-0 rounded">
                     <LazyLoadImage className="z-10 p-1 w-fit" src={projectData.img[1].src} alt={projectData.img[1].alt} />
                 </div>
 
-                <div className="w-2/3 m-4">
+                <div className="md:w-2/3 m-4">
                     <p className="text-justify" dangerouslySetInnerHTML={{ __html: projectData.description }}></p>
                     {projectData.link ? (
                         <a href={projectData.link} target="_blank" className="mt-8 bg-vertt p-2 rounded-xl text-black flex w-fit"><b className="pr-4">Lien vers le projet</b><img src={link} alt="lien vers le site"></img> </a>
@@ -34,13 +39,13 @@ function Project() {
                     }
 
 
-                    <div className="flex mt-8">
+                    <div className="flex flex-col md:flex-row mt-8">
                         <div className="w-1/2">
                             <h5 className="mb-4"><b><u className="decoration-terracota">Outils :</u></b></h5>
                             <ul className="ml-8 mb-4 list-disc">{projectData.tools.map((tool) => (<li className="list-disc" key={tool}> {tool}</li>))}</ul>
 
                         </div>
-                        <div className="w-1/2">
+                        <div className="md:w-1/2">
                             <p className="mt-4 mb-8 decoration-terracota text-end"><span className=" bg-terracota p-2 rounded-xl text-beigee"><b>Année :</b> {projectData.date} ( {projectData.class} )</span> </p>
                             <p className="w-full mb-4 decoration-vertt text-end"><span className=" bg-vertt p-2 rounded-xl text-black"><b>Rôles : </b> {projectData.roles.join(" - ")}</span></p>
                         </div>
@@ -58,6 +63,20 @@ function Project() {
                     <LazyLoadImage className="z-10 p-1 w-fit" src={projectData.img[4].src} alt={projectData.img[4].alt} />
                 </div>
             </div>
+            {projectId.includes("myrole") ? (
+                <div className="flex wrap">
+                    <div className="w-1/3 h-fit m-4 bg-terracota rounded">
+                        <LazyLoadImage className="z-10 p-1 w-fit" src={myRole1} alt="equipe de myrole" />
+                    </div>
+                    <div className="w-1/3 h-fit m-4 bg-vertt rounded">
+                        <LazyLoadImage className="z-10 p-1 w-fit" src={myRole2} alt="equipe de myrole" />
+                    </div>
+                    <div className="w-1/3 h-fit m-4 bg-terracota rounded">
+                        <LazyLoadImage className="z-10 p-1 w-fit" src={myRole3} alt="equipe de myrole" />
+                    </div>
+                </div>
+            ) : null
+            }
 
             <div className="homePart5 home wrap">
                 <div className="transitionHomePart3 flex align-center justify-end -mb-20">
@@ -67,14 +86,14 @@ function Project() {
             </div>
 
             <div className="bgProjectPart3 mb-8">
-                <div className="projectPart3 home wrap flex justify-center items-center">
+                <div className="projectPart3 text-center home wrap flex flex-col md:flex-row justify-center items-center">
                     <div>
-                        <p className="mb-8">{projectData.skill[0]}</p>
-                        <p className="mt-8">{projectData.skill[1]}</p>
+                        <p className="mb-6 md:mb-12">{projectData.skill[0]}</p>
+                        <p>{projectData.skill[1]}</p>
                     </div>
                     <img className="w-1/4" src={sun}></img>
                     <div>
-                        <p>{projectData.skill[2]}</p>
+                        <p className="mb-6 md:mb-12">{projectData.skill[2]}</p>
                         <p>{projectData.skill[3]}</p>
                     </div>
                 </div>
@@ -87,7 +106,7 @@ function Project() {
 
             <div className="flex flex-col wrap">
                 <h5 className="stroke titreProject">Conclusion :</h5>
-                <p className="mx-64 mt-4 text-justify" dangerouslySetInnerHTML={{ __html: projectData.conclusion }}></p>
+                <p className="lg:mx-64 mt-4 text-justify" dangerouslySetInnerHTML={{ __html: projectData.conclusion }}></p>
             </div>
         </>
     );
